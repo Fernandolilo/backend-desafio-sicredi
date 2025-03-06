e713de21b6179f6b5a4302a4011f7f82c00da4af
fernandolilo <fernando.sistenpro@outlook.com>
2025-03-06 09:24:50 -0300

    Interface implementada

---
+++ src/main/java/com/systempro/sessao/service/impl/VoteServiceImpl.java
@@ -0,0 +1,18 @@
+package com.systempro.sessao.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.systempro.sessao.entity.Vote;
+import com.systempro.sessao.service.VoteService;
+
+@Service
+public class VoteServiceImpl implements VoteService{
+
+	@Override
+	public Vote save(Vote vote) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
