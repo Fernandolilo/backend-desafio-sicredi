c752f0fd39694493fdb6287dde88d7419be8cfbf
fernandolilo <fernando.sistenpro@outlook.com>
2025-03-06 18:06:17 -0300

    ajust in AgendaServiceImpl

--- src/main/java/com/systempro/sessao/service/impl/VoteServiceImpl.java
+++ src/main/java/com/systempro/sessao/service/impl/VoteServiceImpl.java
@@ -9,17 +9,17 @@ import com.systempro.sessao.service.VoteService;
 @Service
 public class VoteServiceImpl implements VoteService {
 
-	private final VoteRepository repository;
+    private final VoteRepository repository;
 
-	public VoteServiceImpl(VoteRepository repository) {
-		this.repository = repository;
-	}
+    public VoteServiceImpl(VoteRepository repository) {
+        this.repository = repository;
+    }
 
-	@Override
-	public Vote save(Vote vote) {
-		// TODO Auto-generated method stub
-		return repository.save(vote);
-	}
-
-	
+    @Override
+    public Vote save(Vote vote) {
+    	 if (vote == null || vote.getSession() == null) {
+    	        throw new IllegalArgumentException("Vote or Session cannot be null");
+    	    }
+        return repository.save(vote);
+    }
 }
